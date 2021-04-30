110f9293cf61e0b20498f618f5df0a0c5fe688ab
tmclr <tmclr@yandex.ru>
2021-04-30 16:48:07 +0300

    removed gitignored files

--- public/scripts/controlf.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class control {
-    text: HTMLInputElement;
-    output: HTMLElement;
-    constructor(button: HTMLElement, input: HTMLInputElement, word: HTMLElement);
-    getText(): void;
-}
